1a46d5c29427996ae8968aaa96cb2595ee7d6095
Piotr Fej <p.fejcher@gmail.com>
2019-06-16 10:46:07 +0200

    second commit

--- src/main/java/BMIcalculator.java
+++ src/main/java/BMIcalculator.java
@@ -1,2 +1,54 @@
-public class BMIcalculator {
-}
+
+
+public class BMIcalculator implements Calculator{
+
+    double weight;
+    double height;
+
+    public BMIcalculator(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    @Override
+    public double calculate() {
+        return weight/Math.pow(height, 2);
+    }
+
+    @Override
+    public String interpret() {
+
+        String interpretacja = "Nieprawidłowy wynik.";
+        int bmi = (int)(calculate() * 100);
+
+       if(bmi < 1600){
+           interpretacja = "Wygłodzenie!";}
+
+           else if (bmi >1600 || bmi <= 1699){
+               interpretacja = "Wychudzenie!";
+           }
+
+           else if (bmi > 1699 || bmi <=1849 ){
+               interpretacja = "Niedowaga";
+       }
+           else if (bmi > 1850 || bmi <= 2499){
+               interpretacja = "Wartość prawidlowa";
+       }
+           else if (bmi > 2500 || bmi <= 2999){
+               interpretacja = "Nadwaga";
+       }
+           else if (bmi > 3000 || bmi <= 3499){
+               interpretacja = "I stopień otyłości";
+       }
+           else if (bmi > 3500 || bmi <= 3999){
+               interpretacja = "II stopień otyłości (otyłość kliniczna";
+       }
+           else if (bmi >= 4000){
+               interpretacja = "III stopień otyłości (otyłośc skrajna)";
+       }
+
+           return interpretacja;
+       }
+
+    }
+
--- src/test/java/BMIcalculatorTest.java
+++ src/test/java/BMIcalculatorTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
 public class BMIcalculatorTest {
+
+    private BMIcalculator bmIcalculator;
+    private static final double DELTA = 1e-2;
+
+    /*@Before
+    public void setup(){
+        bmIcalculator = new BMIcalculator();
+    }*/
+
+    @Test
+    public void bmi_for_heigh_180cm_weigh_79kg(){
+        double weigh = 79;
+        double heigh = 1.80;
+        double expectedBMI = 24.38;
+
+        assertEquals(expectedBMI, new BMIcalculator(weigh, heigh).calculate(), DELTA);
+    }
+
+    @Test
+    public void bmi_for_heigh_200cm_weigh_300kg(){
+        double weigh = 300;
+        double heigh = 2.0;
+        double expectedBMI = 75;
+
+        assertEquals(expectedBMI, new BMIcalculator(weigh, heigh).calculate(), DELTA);
+    }
+
+    @Test
+    public void interpret_for_bmi_20(){
+
+    }
 }
